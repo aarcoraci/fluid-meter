a5b82ac7a5b8c250edb06480351510541728402b
Angel Arcoraci <angel.arcoraci@gmail.com>
2022-02-09 16:21:31 +0100

    build fixes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
   return {
+    base: mode === 'production' ? '/fluid-meter/' : '/',
     build: {
       outDir: './docs'
     }
